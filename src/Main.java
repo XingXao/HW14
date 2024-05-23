52f8e8246c27c3d6fdbef236ec1788b39d21162c
XingXao <xandrnf@gmail.com>
2024-05-23 20:46:32 +0500

    done

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,13 @@ public class Main {
     }
 
     public static double avg(int num) {
-        //todo Дописать логику работы метода сюда. Метод main не трогаем!!!
-        return 0; //todo заменить 0 на корректный результат.
+        String strNum = String.valueOf(num);
+        double dobleNum = 0;
+
+        for (int i = 0; i < strNum.length(); i++) {
+            dobleNum = dobleNum + Integer.parseInt(String.valueOf(strNum.charAt(i)));
+        }
+
+        return dobleNum / strNum.length();
     }
 }
